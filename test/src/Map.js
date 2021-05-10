e63ab9b2352aff89cf79c5b39cf916895b4e05c4
Andrew DeChristopher <andrewdechristopher@gmail.com>
2021-05-10 10:54:19 -0400

    fix: downgraded mapbox to remove need for token

--- test/src/Map.js
+++ test/src/Map.js
@@ -20,19 +20,6 @@ const Map = () => {
     // Add zoom and rotation controls to the map.
     map.addControl(new mapboxgl.NavigationControl());
 
-    map.on("load", function() {
-      // optional 3d terrain, you will need hillshading as well
-      // map.addSource("mapbox-dem", {
-      //   "type": "raster-dem",
-      //   "url": "mapbox://mapbox.mapbox-terrain-dem-v1",
-      //   "tileSize": 512,
-      //   "maxzoom": 14
-      // });
-
-      // // add the DEM source as a terrain layer with exaggerated height
-      // map.setTerrain({ "source": "mapbox-dem", "exaggeration": 1.5 });
-    });
-
     // Clean up on unmount
     return () => map.remove();
   }, []); // eslint-disable-line react-hooks/exhaustive-deps
